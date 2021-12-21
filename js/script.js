351e4f2e192042ebd688947f1d1910883111a46d
William Riberack <riberackw@gmail.com>
2021-12-22 00:52:23 +0100

    NewFEUP

---
+++ js/script.js
@@ -0,0 +1,3 @@
+function alertIncomplete() {
+    alert("Este site é experimental e não está integrado com o login e suas funções ou outras partes do site original. Feedback é bem-vindo e altamente recomendado, envie-o para riberackw@gmail.com");
+  }
